fa6a911d11eff5f336cdad563ae1ad5168698da1
Lucas Hohmann <lfhohmann@gmail.com>
2022-02-22 05:51:09 -0300

    Moved constants back to the notebook

--- const.py
+++
@@ -1,5 +0,0 @@
-NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]  # List of valid numbers
-OPERATORS = ["/", "*", "+", "-"]  # List of valid operators
-NEGATIVE_OPERATORS = ["-"]  # '-' operator
-
-EMPTY_STRING = ""  # An empty string
